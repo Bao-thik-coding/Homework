6e162184942fca49106a0c1542f42017a506f15a
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-06 20:12:48 +0700

    sa

--- git/Buoi3/Buoi3/Properties/AssemblyInfo.cs
+++ git/Buoi3/Buoi3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Buoi3")]
+[assembly: AssemblyTitle("BUOI3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Buoi3")]
+[assembly: AssemblyProduct("BUOI3")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f3d92cb-e1cd-4261-bbca-638582908b9f")]
+[assembly: Guid("4909b20a-dbba-4b5e-b2f6-652e93d6f72a")]
 
 // Version information for an assembly consists of the following four values:
 //
