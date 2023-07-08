3768b328e56917713be2b1061337fb52ea8e772a
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-08 21:22:27 +0700

    ahi

---
+++ Lonnhat/Lonnhat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lonnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vitri = lstInpt.IndexOf(find);
+            if (vitri >= 0) Console.WriteLine("TIm thay tai thu {0}", vitri);
+            else Console.WriteLine("Ko tim thay");
+
+            int max_value = lstInpt.Max();
+            int max_index = lstInpt.Max();
+            Console.WriteLine("gia tri max={1}, tai thu {0}", max_index, max_value);
+        }
+    }
+}
