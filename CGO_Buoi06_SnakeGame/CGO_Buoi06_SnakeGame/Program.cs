3aff4b4ae043a758adacdc67188b32bb4da3b560
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-14 20:04:01 +0700

    hihi

---
+++ CGO_Buoi06_SnakeGame/CGO_Buoi06_SnakeGame/Program.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi06_SnakeGame
+{
+    class Program
+    {
+        // Parameters
+        public Random rand = new Random();
+        public ConsoleKeyInfo keypress = new ConsoleKeyInfo();
+        int score, headX, headY, fruitX, fruitY, nTail;
+        int[] TailX = new int[100];
+        int[] TailY = new int[100];
+        const int height = 20;
+        const int width = 60;
+        const int panel = 10;
+        bool gameOver, reset, isprinted, horizontal, vertical;
+        string dir, pre_dir;
+        int typeFruit;
+        ConsoleColor fruitColor;
+        int snakeSpeed;
+        int highScore;
+
+        // Show the start banner
+        void ShowBanner()
+        {
+            Console.SetWindowSize(width, height + panel);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.CursorVisible = false;
+            Console.WriteLine("=== SNAKE GAME ===");
+            Console.WriteLine("How to play:");
+            Console.WriteLine("- Press any key to start");
+            Console.WriteLine("- Press P to pause the game");
+            Console.WriteLine("- Press R to restart the game");
+            Console.WriteLine("- Press Q to quit the game");
+
+            keypress = Console.ReadKey();
+            if (keypress.Key == ConsoleKey.Q) Environment.Exit(0);
+        }
+
+        // Initialize the game
+        void Setup()
+        {
+            dir = "RIGHT";
+            pre_dir = "";
+            score = 0;
+            nTail = 0;
+            gameOver = reset = isprinted = false;
+            headX = 30;
+            headY = 10;
+            randomQua();
+            typeFruit = rand.Next(1, 3);
+            SetFruitColor();
+            snakeSpeed = 100;
+        }
+
+        // Generate random fruit coordinates
+        void randomQua()
+        {
+            fruitX = rand.Next(1, width - 1);
+            fruitY = rand.Next(1, height - 1);
+        }
+
+        // Set color for the fruit based on its type
+        void SetFruitColor()
+        {
+            switch (typeFruit)
+            {
+                case 1:
+                    fruitColor = ConsoleColor.Red;
+                    break;
+                case 2:
+                    fruitColor = ConsoleColor.Yellow;
+                    break;
+                // Add more cases for additional fruit types
+                default:
+                    fruitColor = ConsoleColor.Red;
+                    break;
+            }
+        }
+
+        // Update the game state
+        void Update()
+        {
+            while (!gameOver)
+            {
+                CheckInput();
+                Logic();
+                Render();
+
+                if (reset) break;
+                Thread.Sleep(snakeSpeed);
+            }
+
+            if (gameOver) Lose();
+        }
+
+        // Check for player input
+        void CheckInput()
+        {
+            while (Console.KeyAvailable)
+            {
+                keypress = Console.ReadKey();
+                pre_dir = dir;
+                switch (keypress.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        dir = "LEFT";
+                        break;
+                    case ConsoleKey.RightArrow:
+                        dir = "RIGHT";
+                        break;
+                    case ConsoleKey.DownArrow:
+                        dir = "DOWN";
+                        break;
+                    case ConsoleKey.UpArrow:
+                        dir = "UP";
+                        break;
+                    case ConsoleKey.P:
+                        dir = "STOP";
+                        break;
+                    case ConsoleKey.Q:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        // Handle game logic
+        void Logic()
+        {
+            int preX = TailX[0], preY = TailY[0];
+            int tempX, tempY;
+
+            if (dir != "STOP")
+            {
+                TailX[0] = headX;
+                TailY[0] = headY;
+
+                for (int i = 1; i < nTail; i++)
+                {
+                    tempX = TailX[i];
+                    tempY = TailY[i];
+                    TailX[i] = preX;
+                    TailY[i] = preY;
+                    preX = tempX;
+                    preY = tempY;
+                }
+            }
+
+            switch (dir)
+            {
+                case "LEFT":
+                    headX--;
+                    break;
+                case "RIGHT":
+                    headX++;
+                    break;
+                case "UP":
+                    headY--;
+                    break;
+                case "DOWN":
+                    headY++;
+                    break;
+                case "STOP":
+                    PauseGame();
+                    dir = pre_dir;
+                    break;
+            }
+
+            if (headX <= 0)
+                headX = width - 2;
+            else if (headX >= width - 1)
+                headX = 1;
+            if (headY <= 0)
+                headY = height - 2;
+            else if (headY >= height - 1)
+                headY = 1;
+
+            gameOver = CheckCollision();
+
+            if (headX == fruitX && headY == fruitY)
+            {
+                score += GetFruitScore();
+                nTail++;
+                randomQua();
+                typeFruit = rand.Next(1, 3);
+                SetFruitColor();
+                AdjustSpeed();
+            }
+        }
+
+        // Check for collisions
+        bool CheckCollision()
+        {
+            for (int i = 1; i < nTail; i++)
+            {
+                if (headX == TailX[i] && headY == TailY[i])
+                    return true;
+            }
+            return false;
+        }
+
+        // Get the score for the current fruit type
+        int GetFruitScore()
+        {
+            switch (typeFruit)
+            {
+                case 1:
+                    return 2;
+                case 2:
+                    return 3;
+                // Add more cases for additional fruit types
+                default:
+                    return 2;
+            }
+        }
+
+        // Adjust snake speed based on the score
+        void AdjustSpeed()
+        {
+            if (score < 20)
+                snakeSpeed = 100;
+            else if (score < 50)
+                snakeSpeed = 80;
+            else if (score < 100)
+                snakeSpeed = 60;
+            else
+                snakeSpeed = 40;
+        }
+
+        // Render the game board
+        void Render()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("#");
+                    }
+                    else if (j == 0 || j == width - 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("#");
+                    }
+                    else if (fruitX == j && fruitY == i)
+                    {
+                        Console.ForegroundColor = fruitColor;
+                        Console.Write("?");
+                    }
+                    else if (headX == j && headY == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        isprinted = false;
+                        for (int k = 0; k < nTail; k++)
+                        {
+                            if (TailX[k] == j && TailY[k] == i)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write("o");
+                                isprinted = true;
+                            }
+                        }
+                        if (!isprinted)
+                            Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("High Score: " + highScore);
+        }
+
+        // Pause the game
+        void PauseGame()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== SNAKE GAME ===");
+                Console.WriteLine("Game paused!");
+                Console.WriteLine("- Press P to resume");
+                Console.WriteLine("- Press R to restart");
+                Console.WriteLine("- Press Q to quit");
+
+                keypress = Console.ReadKey();
+                if (keypress.Key == ConsoleKey.Q)
+                    Environment.Exit(0);
+                if (keypress.Key == ConsoleKey.R)
+                {
+                    reset = true;
+                    break;
+                }
+                if (keypress.Key == ConsoleKey.P)
+                    break;
+            }
+        }
+
+        // Handle game over
+        void Lose()
+        {
+            Console.WriteLine("=== SNAKE GAME ===");
+            Console.WriteLine("Game over!");
+            Console.WriteLine("- Press R to restart");
+            Console.WriteLine("- Press Q to quit");
+
+            if (score > highScore)
+                highScore = score;
+
+            while (true)
+            {
+                keypress = Console.ReadKey();
+                if (keypress.Key == ConsoleKey.Q)
+                    Environment.Exit(0);
+                if (keypress.Key == ConsoleKey.R)
+                {
+                    reset = true;
+                    break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program snakegame = new Program();
+            snakegame.ShowBanner();
+            while (true)
+            {
+                snakegame.Setup();
+                snakegame.Update();
+                Console.Clear();
+            }
+        }
+    }
+}
--- Mang/Mang/Program.cs
+++ Mang/Mang/Program.cs
@@ -10,70 +10,318 @@ namespace Mang
     {
         static void Main(string[] args)
         {
-            string[,] map = {
-            {"*", ".", ".", "."},
-            {".", ".", ".", "."},
-            {".", "*", ".", "."},
-            {".", ".", ".", "."}
-        };
-            int MAP_HEIGHT = map.GetLength(0);
-            int MAP_WIDTH = map.GetLength(1);
+            static readonly int gridW = 90;
+            static readonly int gridH = 25;
+            static Cell[,] grid = new Cell[gridH, gridW];
+            static Cell currentCell;
+            static Cell food;
+            static int FoodCount;
+            static int direction; //0=Up 1=Right 2=Down 3=Left
+            static readonly int speed = 1;
+            static bool Populated = false;
+            static bool Lost = false;
+            static int snakeLength;
 
-            string[,] mapReport = new string[MAP_HEIGHT, MAP_WIDTH];
-            for (int yOrdinate = 0; yOrdinate < MAP_HEIGHT; yOrdinate++)
+            static void Main(string[] args)
             {
-                for (int xOrdinate = 0; xOrdinate < map.GetLength(0); xOrdinate++)
+                if (!Populated)
                 {
-                    string curentCell = map[yOrdinate, xOrdinate];
-                    if (curentCell.Equals("*"))
+                    FoodCount = 0;
+                    snakeLength = 5;
+                    populateGrid();
+                    currentCell = grid[(int)Math.Ceiling((double)gridH / 2), (int)Math.Ceiling((double)gridW / 2)];
+                    updatePos();
+                    addFood();
+                    Populated = true;
+                }
+
+                while (!Lost)
+                {
+                    Restart();
+                }
+            }
+
+            static void Restart()
+            {
+                Console.SetCursorPosition(0, 0);
+                printGrid();
+                Console.WriteLine("Length: {0}", snakeLength);
+                getInput();
+            }
+
+            static void updateScreen()
+            {
+                Console.SetCursorPosition(0, 0);
+                printGrid();
+                Console.WriteLine("Length: {0}", snakeLength);
+            }
+
+            static void getInput()
+            {
+
+                //Console.Write("Where to move? [WASD] ");
+                ConsoleKeyInfo input;
+                while (!Console.KeyAvailable)
+                {
+                    Move();
+                    updateScreen();
+                }
+                input = Console.ReadKey();
+                doInput(input.KeyChar);
+            }
+
+            static void checkCell(Cell cell)
+            {
+                if (cell.val == "%")
+                {
+                    eatFood();
+                }
+                if (cell.visited)
+                {
+                    Lose();
+                }
+            }
+
+            static void Lose()
+            {
+                Console.WriteLine("\n You lose!");
+                Thread.Sleep(1000);
+                Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(-1);
+            }
+
+            static void doInput(char inp)
+            {
+                switch (inp)
+                {
+                    case 'w':
+                        goUp();
+                        break;
+                    case 's':
+                        goDown();
+                        break;
+                    case 'a':
+                        goRight();
+                        break;
+                    case 'd':
+                        goLeft();
+                        break;
+                }
+            }
+
+            static void addFood()
+            {
+                Random r = new Random();
+                Cell cell;
+                while (true)
+                {
+                    cell = grid[r.Next(grid.GetLength(0)), r.Next(grid.GetLength(1))];
+                    if (cell.val == " ")
+                        cell.val = "%";
+                    break;
+                }
+            }
+
+            static void eatFood()
+            {
+                snakeLength += 1;
+                addFood();
+            }
+
+            static void goUp()
+            {
+                if (direction == 2)
+                    return;
+                direction = 0;
+            }
+
+            static void goRight()
+            {
+                if (direction == 3)
+                    return;
+                direction = 1;
+            }
+
+            static void goDown()
+            {
+                if (direction == 0)
+                    return;
+                direction = 2;
+            }
+
+            static void goLeft()
+            {
+                if (direction == 1)
+                    return;
+                direction = 3;
+            }
+
+            static void Move()
+            {
+                if (direction == 0)
+                {
+                    //up
+                    if (grid[currentCell.y - 1, currentCell.x].val == "*")
                     {
-                        mapReport[yOrdinate, xOrdinate] = "*";
+                        Lose();
+                        return;
                     }
-                    else
+                    visitCell(grid[currentCell.y - 1, currentCell.x]);
+                }
+                else if (direction == 1)
+                {
+                    //right
+                    if (grid[currentCell.y, currentCell.x - 1].val == "*")
                     {
-                        int[,] NEIGHBOURS_ORDINATE = {
-                        {yOrdinate - 1, xOrdinate - 1}, {yOrdinate - 1, xOrdinate}, {yOrdinate - 1, xOrdinate + 1},
-                        {yOrdinate, xOrdinate - 1}, {yOrdinate, xOrdinate + 1},
-                        {yOrdinate + 1, xOrdinate - 1}, {yOrdinate + 1, xOrdinate}, {yOrdinate + 1, xOrdinate + 1},};
+                        Lose();
+                        return;
+                    }
+                    visitCell(grid[currentCell.y, currentCell.x - 1]);
+                }
+                else if (direction == 2)
+                {
+                    //down
+                    if (grid[currentCell.y + 1, currentCell.x].val == "*")
+                    {
+                        Lose();
+                        return;
+                    }
+                    visitCell(grid[currentCell.y + 1, currentCell.x]);
+                }
+                else if (direction == 3)
+                {
+                    //left
+                    if (grid[currentCell.y, currentCell.x + 1].val == "*")
+                    {
+                        Lose();
+                        return;
+                    }
+                    visitCell(grid[currentCell.y, currentCell.x + 1]);
+                }
+                Thread.Sleep(speed * 100);
+            }
 
-                        int minesAround = 0;
-                        int length = NEIGHBOURS_ORDINATE.GetLength(0);
-                        for (int i = 0; i < length; i++)
-                        {
-                            int xOrdinateOfNeighbour = NEIGHBOURS_ORDINATE[i, 1];
-                            int yOrdinateOfNeighbour = NEIGHBOURS_ORDINATE[i, 0];
+            static void visitCell(Cell cell)
+            {
+                currentCell.val = "#";
+                currentCell.visited = true;
+                currentCell.decay = snakeLength;
+                checkCell(cell);
+                currentCell = cell;
+                updatePos();
 
-                            bool isOutOfMapNeighbour = xOrdinateOfNeighbour < 0
-                                    || xOrdinateOfNeighbour == MAP_WIDTH
-                                    || yOrdinateOfNeighbour < 0
-                                    || yOrdinateOfNeighbour == MAP_HEIGHT;
-                            if (isOutOfMapNeighbour)
-                            {
-                                continue;
-                            }
+                //checkCell(currentCell);
+            }
 
-                            bool isMineOwnerNeighbour = map[yOrdinateOfNeighbour, xOrdinateOfNeighbour].Equals("*");
-                            if (isMineOwnerNeighbour)
-                            {
-                                minesAround++;
-                            }
-                        }
+            static void updatePos()
+            {
 
-                        mapReport[yOrdinate, xOrdinate] = minesAround.ToString();
+                currentCell.Set("@");
+                if (direction == 0)
+                {
+                    currentCell.val = "^";
+                }
+                else if (direction == 1)
+                {
+                    currentCell.val = "<";
+                }
+                else if (direction == 2)
+                {
+                    currentCell.val = "v";
+                }
+                else if (direction == 3)
+                {
+                    currentCell.val = ">";
+                }
+
+                currentCell.visited = false;
+                return;
+            }
+
+            static void populateGrid()
+            {
+                Random random = new Random();
+                for (int col = 0; col < gridH; col++)
+                {
+                    for (int row = 0; row < gridW; row++)
+                    {
+                        Cell cell = new Cell();
+                        cell.x = row;
+                        cell.y = col;
+                        cell.visited = false;
+                        if (cell.x == 0 || cell.x > gridW - 2 || cell.y == 0 || cell.y > gridH - 2)
+                            cell.Set("*");
+                        else
+                            cell.Clear();
+                        grid[col, row] = cell;
                     }
                 }
             }
 
-            for (int yOrdinate = 0; yOrdinate < MAP_HEIGHT; yOrdinate++)
+            static void printGrid()
             {
-                Console.WriteLine("\n");
-                for (int xOrdinate = 0; xOrdinate < MAP_WIDTH; xOrdinate++)
+                string toPrint = "";
+                for (int col = 0; col < gridH; col++)
                 {
-                    String currentCellReport = mapReport[yOrdinate, xOrdinate];
-                    Console.Write(currentCellReport);
+                    for (int row = 0; row < gridW; row++)
+                    {
+                        grid[col, row].decaySnake();
+                        toPrint += grid[col, row].val;
+
+                    }
+                    toPrint += "\n";
+                }
+                Console.WriteLine(toPrint);
+            }
+  public class Cell
+        {
+            public string val
+            {
+                get;
+                set;
+            }
+            public int x
+            {
+                get;
+                set;
+            }
+            public int y
+            {
+                get;
+                set;
+            }
+            public bool visited
+            {
+                get;
+                set;
+            }
+            public int decay
+            {
+                get;
+                set;
+            }
+
+            public void decaySnake()
+            {
+                decay -= 1;
+                if (decay == 0)
+                {
+                    visited = false;
+                    val = " ";
                 }
             }
-            Console.ReadLine();
+
+            public void Clear()
+            {
+                val = " ";
+            }
+
+            public void Set(string newVal)
+            {
+                val = newVal;
+            }
         }
+    }
+}
     }
 }
